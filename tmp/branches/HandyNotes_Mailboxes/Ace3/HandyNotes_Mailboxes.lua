613b386e15c7766a8f3200f4bd1a3238fb8e853c
xinhuan <xinhuan@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2008-02-13 21:05:17 +0000

    HandyNotes_Mailboxes: Fix various drycode issues.

--- tmp/branches/HandyNotes_Mailboxes/Ace3/HandyNotes_Mailboxes.lua
+++ tmp/branches/HandyNotes_Mailboxes/Ace3/HandyNotes_Mailboxes.lua
@@ -1,9 +1,8 @@
 ---------------------------------------------------------
 -- Addon declaration
 HandyNotes_Mailboxes = LibStub("AceAddon-3.0"):NewAddon("HandyNotes_Mailboxes","AceEvent-3.0")
-local MB = HandyNotes_Mailboxes
-local Astrolabe = DongleStub("Astrolabe-0.4")
---local L = LibStub("AceLocale-3.0"):GetLocale("HandyNotes_FlightMasters", false)
+local HMB = HandyNotes_Mailboxes
+--local L = LibStub("AceLocale-3.0"):GetLocale("HandyNotes_Mailboxes", false)
 
 
 ---------------------------------------------------------
@@ -12,9 +11,9 @@ local db
 local defaults = {
 	global = {
 		mailboxes = {
-			["*"] = ["*"],
-		}
-	}
+			["*"] = {},  -- [mapFile] = {[coord] = true, [coord] = true}
+		},
+	},
 	profile = {
 		icon_scale         = 1.0,
 		icon_alpha         = 1.0,
@@ -24,12 +23,30 @@ local defaults = {
 -- Localize some globals
 local next = next
 local GameTooltip = GameTooltip
+local WorldMapTooltip = WorldMapTooltip
 
 
 ---------------------------------------------------------
 -- Constants
 local icon = "Interface\\Icons\\INV_Letter_15"
-local MBHandler = {}
+
+
+---------------------------------------------------------
+-- Plugin Handlers to HandyNotes
+local HMBHandler = {}
+function HMBHandler:OnEnter(mapFile, coord)
+	if ( self:GetCenter() > self:GetParent():GetCenter() ) then -- compare X coordinate
+		WorldMapTooltip:SetOwner(self, "ANCHOR_LEFT")
+	else
+		WorldMapTooltip:SetOwner(self, "ANCHOR_RIGHT")
+	end
+	WorldMapTooltip:SetText("Mailbox")
+	WorldMapTooltip:Show()
+end
+
+function HMBHandler:OnLeave(mapFile, coord)
+	WorldMapTooltip:Hide()
+end
 
 do
 	-- This is a custom iterator we use to iterate over every node in a given zone
@@ -38,31 +55,48 @@ do
 		local state, value = next(t, prestate)
 		while state do -- Have we reached the end of this zone?
 			if value then
-				return state, icon, db.icon_scale,db.icon_alpha
+				return state, icon, db.icon_scale, db.icon_alpha
 			end
 			state, value = next(t, state) -- Get next data
 		end
 		return nil, nil, nil, nil
 	end
 	function HMBHandler:GetNodes(mapFile)
-		return iter, self.db.global.mailboxes[mapFile], nil
+		return iter, HMB.db.global.mailboxes[mapFile], nil
 	end
 end
+
+-- TODO: Add right click menu to delete a mailbox
+
+
+---------------------------------------------------------
+-- Core functions
+
+function HMB:AddMailBox()
+	SetMapToCurrentZone()
+	local x, y = GetPlayerMapPosition("player")
+	local coord = HandyNotes:getCoord(x, y)
+	local mapFile = GetMapInfo()
+	-- TODO: Add code to remove nearby mailboxes within about 15 yards
+	self.db.global.mailboxes[mapFile][coord] = true
+	HMB:SendMessage("HandyNotes_NotifyUpdate", "Mailboxes")
+end
+
 
 ---------------------------------------------------------
 -- Options table
 local options = {
 	type = "group",
-	name = "FlightMasters",
-	desc = "FlightMasters",
+	name = "Mailboxes",
+	desc = "Mailboxes",
 	get = function(info) return db[info.arg] end,
 	set = function(info, v)
 		db[info.arg] = v
-		HFM:SendMessage("HandyNotes_NotifyUpdate", "Mailboxes")
+		HMB:SendMessage("HandyNotes_NotifyUpdate", "Mailboxes")
 	end,
 	args = {
 		desc = {
-			name = "These settings control the look and feel of the Mailbox icon.",
+			name = "These settings control the look and feel of the Mailbox icon. Note that HandyNotes_MailBoxes does not come with any precompiled data, when you visit mailboxes, it will automatically add the data into your database.",
 			type = "description",
 			order = 0,
 		},
@@ -84,25 +118,21 @@ local options = {
 		},
 	},
 }
+
+
+---------------------------------------------------------
+-- Addon initialization, enabling and disabling
+
 function HMB:OnInitialize()
 	-- Set up our database
 	self.db = LibStub("AceDB-3.0"):New("HandyNotes_MailboxesDB", defaults)
 	db = self.db.profile
 	-- Initialize our database with HandyNotes
-	HandyNotes:RegisterPluginDB("Mailboxes", MBHandler, options)
+	HandyNotes:RegisterPluginDB("Mailboxes", HMBHandler, options)
 end
 
 function HMB:OnEnable()
-	self:RegisterEvent("MAIL_SHOW", function() 
-		local x, y = GetPlayerMapPosition("player")	
-		local coord = HandyNotes:getCoord(x,y)
-		local continent = GetCurrentMapContinent()
-		local map = HandyNotes:GetMapFile(continent)
-		if map then
-			self.db.global.mailboxes[map][coord]=true
-			HFM:SendMessage("HandyNotes_NotifyUpdate", "Mailboxes")
-		end
-	end)
+	self:RegisterEvent("MAIL_SHOW", "AddMailBox")
 end
 
 function HMB:OnDisable()
