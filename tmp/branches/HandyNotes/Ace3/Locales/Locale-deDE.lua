7b77fe8f7ff956381412028532121d82d684126a
ensabahnur <ensabahnur@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2008-03-03 11:07:06 +0000

    HandyNotes: - add deDE Locale

--- tmp/branches/HandyNotes/Ace3/Locales/Locale-deDE.lua
+++ tmp/branches/HandyNotes/Ace3/Locales/Locale-deDE.lua
@@ -4,5 +4,39 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes", "deDE")
 if not L then return end
 
+L["HandyNotes"] = "HandyNotes"
 
+L["Enable HandyNotes"] = "HandyNotes aktivieren"
+L["Enable or disable HandyNotes"] = "HandyNotes aktivieren oder deaktivieren"
+
+L["Overall settings"] = "Allgemeine Einstellungen"
+L["Overall settings that affect every database"] = "Allgemeine Einstellungen für jede Datenbank"
+L["These settings control the look and feel of HandyNotes globally. The icon's scale and alpha here are multiplied with the plugin's scale and alpha."] = true
+L["World Map Icon Scale"] = "Skalierung des Weltkarten-Icons"
+L["The overall scale of the icons on the World Map"] = "Die allgemeine Skalierung der Icons auf der Weltkarte"
+L["World Map Icon Alpha"] = "Alpha-Transparenz für Weltkarten-Icons"
+L["The overall alpha transparency of the icons on the World Map"] = "Die allgemeine Alpha-Transparenz der Icons auf der Weltkarte"
+L["Minimap Icon Scale"] = "Minikarten Icon-Skalierung"
+L["The overall scale of the icons on the Minimap"] = "Die allgemeine Skalierung der Icons auf der Minikarte"
+L["Minimap Icon Alpha"] = "Alpha-Transparens für Minimap-Icons"
+L["The overall alpha transparency of the icons on the Minimap"] = "Die allgemeine Alpha-Transparenz der Icons auf der Minikarte"
+
+L["Plugins"] = "Plugins"
+L["Plugin databases"] = "Plugin Datenbanken"
+L["Configuration for each individual plugin database."] = "Konfiguration für jede Plugin Datenbank."
+
+L["Add Handy Note"] = "Notiz hinzufügen"
+L["Edit Handy Note"] = "Notiz bearbeiten"
+L["Delete Handy Note"] = "Notiz löschen"
+L["Title"] = "Titel"
+L["Description/Notes:"] = "Beschreibung/Notizen:"
+L["Show on continent map"] = "Auf Kontinentalkarte zeigen"
+L["Add this location to Cartographer_Waypoints"] = "Diesen Ort bei Cartographer_Waypoints hinzufügen"
+L["|cFF00FF00Hint: |cffeda55fCtrl+Shift+LeftDrag|cFF00FF00 to move a note"] = "|cFF00FF00Hinweis: |cffeda55fStrg+Shift+Linksklick|cFF00FF00 um eine Notiz zu bewegen"
+
+L["These settings control the look and feel of the HandyNotes icons."] = "Diese Einstellungen steuern das aussehen der HandyNotes Icons."
+L["Icon Scale"] = "Icon-Skalierung"
+L["The scale of the icons"] = "Die Skalierung der Icons"
+L["Icon Alpha"] = "Icon-Alpha"
+L["The alpha transparency of the icons"] = "Die Alpha-Transparenz der Icons"
 -- vim: ts=4 noexpandtab
