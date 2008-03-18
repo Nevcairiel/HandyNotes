2d5db1030cdc122e3393a75ace7e6a05292f11bf
next96 <next96@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2008-03-18 17:10:03 +0000

    HandyNotes_Directions: - Add locale koKR

---
+++ tmp/branches/HandyNotes_Directions/Ace3/koKR.lua
@@ -0,0 +1,6 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_Directions", "koKR")
+
+L["A profession trainer"] = "전문기술 상급자"
+L["Trainer: "] = "전문기술: "
+L["A class trainer"] = "직업 상급자"
+L["Class: "] = "직업: "
