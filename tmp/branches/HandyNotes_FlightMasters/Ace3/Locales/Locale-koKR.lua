1bd6fa0c6fdde64ffd06efe63cf0a53bcee44f65
next96 <next96@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2008-03-18 17:15:36 +0000

    HandyNotes_FlightMasters: - Add locale koKR

--- tmp/branches/HandyNotes_FlightMasters/Ace3/Locales/Locale-koKR.lua
+++ tmp/branches/HandyNotes_FlightMasters/Ace3/Locales/Locale-koKR.lua
@@ -4,5 +4,33 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_FlightMasters", "koKR")
 if not L then return end
 
+-- L["HandyNotes_FlightMasters"] = true
 
+-- Tooltip descriptions on mouseover
+L["Alliance FlightMaster"] = "얼라이언스 비행조련사"
+L["Horde FlightMaster"] = "호드 비행조련사"
+L["Neutral FlightMaster"] = "중립 비행조련사"
+L["Druid FlightMaster"] = "드루이드 비행조련사"
+L["PvP FlightMaster"] = "PvP 비행조련사"
+L["Aldor FlightMaster"] = "알도르사제회 비행조련사"
+L["Scryer FlightMaster"] = "점술가길드 비행조련사"
+L["Skyguard FlightMaster"] = "하늘경비대 비행조련사"
+
+-- Configuration menus
+L["FlightMasters"] = "비행조련사"
+L["These settings control the look and feel of the FlightMaster icons."] = "비행조련사 아이콘에 대한 보기 설정입니다."
+L["FlightMaster Icons"] = "비행조련사 아이콘"
+L["Icon Scale"] = "아이콘 크기"
+L["The scale of the icons"] = "아이콘의 크기를 변경합니다."
+L["Icon Alpha"] = "아이콘 투명도"
+L["The alpha transparency of the icons"] = "아이콘의 투명도를 변경합니다."
+L["Show both factions"] = "양쪽 평판 보기"
+L["Show all flightmasters instead of only those that you can use"] = "모든 비행조련사를 표시합니다."
+L["Show on continent maps"] = "대륙지도에 표시"
+L["Show flightmasters on continent level maps as well"] = "대륙맵에 비행조련사를 표시합니다."
+L["Flight path lines"] = "비행경로"
+L["Show flight path lines"] = "비행경로 표시"
+L["Show flight path lines on the world map"] = "월드맵에 비행경로를 표시합니다."
+L["Show in zones"] = "지역맵에 표시"
+L["Show flight path lines on the zone maps as well"] = "지역맵에 비행경로를 표시합니다."
 -- vim: ts=4 noexpandtab
