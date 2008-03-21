68a503545c9cf994cbe60ed1e34efe6bd22eebac
nevcairiel <nevcairiel@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2008-03-21 21:56:43 +0000

    HandyNotes_Directions: fix error in koKR (can has shotgun now?)

--- tmp/branches/HandyNotes_Directions/Ace3/koKR.lua
+++ tmp/branches/HandyNotes_Directions/Ace3/koKR.lua
@@ -1,5 +1,5 @@
 ﻿local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_Directions", "koKR")
-
+if not L then return end
 L["A profession trainer"] = "전문기술 상급자"
 L["Trainer: "] = "전문기술: "
 L["A class trainer"] = "직업 상급자"
