5611004d251b7604d2cbd2835588a0613eaa3cbb
xinhuan <xinhuan@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2008-04-06 06:09:29 +0000

    HandyNotes_Mailboxes: Update to use Astrolabe-0.4-NC instead of Astrolabe-0.4.

--- tmp/branches/HandyNotes_Mailboxes/Ace3/HandyNotes_Mailboxes.lua
+++ tmp/branches/HandyNotes_Mailboxes/Ace3/HandyNotes_Mailboxes.lua
@@ -3,7 +3,7 @@
 HandyNotes_Mailboxes = LibStub("AceAddon-3.0"):NewAddon("HandyNotes_Mailboxes","AceEvent-3.0")
 local HMB = HandyNotes_Mailboxes
 local HandyNotes = LibStub("AceAddon-3.0"):GetAddon("HandyNotes")
-local Astrolabe = DongleStub("Astrolabe-0.4")
+local Astrolabe = DongleStub("Astrolabe-0.4-NC")
 --local L = LibStub("AceLocale-3.0"):GetLocale("HandyNotes_Mailboxes", false)
 
 
