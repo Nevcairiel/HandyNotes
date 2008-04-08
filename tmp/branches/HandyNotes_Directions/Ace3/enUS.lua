2c8733105033c8f370c2014ce98d70c807c71efa
xinhuan <xinhuan@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2008-04-08 09:18:19 +0000

    HandyNotes_Directions: Delete Ace3 branch so that author updates to trunk.

--- tmp/branches/HandyNotes_Directions/Ace3/enUS.lua
+++
@@ -1,6 +0,0 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_Directions", "enUS", true)
-
-L["A profession trainer"] = true
-L["Trainer: "] = true
-L["A class trainer"] = true
-L["Class: "] = true
--- tmp/branches/HandyNotes_Directions/Ace3/koKR.lua
+++
@@ -1,6 +0,0 @@
-﻿local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_Directions", "koKR")
-if not L then return end
-L["A profession trainer"] = "전문기술 상급자"
-L["Trainer: "] = "전문기술: "
-L["A class trainer"] = "직업 상급자"
-L["Class: "] = "직업: "
