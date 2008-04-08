cb00a87308aa1f65a5092e229514d45cda33dec8
xinhuan <xinhuan@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2008-04-08 09:18:58 +0000

    HandyNotes_FlightMasters: Delete Ace3 branch.

--- tmp/branches/HandyNotes_FlightMasters/Ace3/Locales/Locale-deDE.lua
+++
@@ -1,8 +0,0 @@
-﻿-- HandyNotes_FlightMasters
--- deDE Localization file
-
-local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_FlightMasters", "deDE")
-if not L then return end
-
-
--- vim: ts=4 noexpandtab
