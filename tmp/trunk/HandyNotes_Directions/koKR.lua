744fd3369ce516a5eb9a363af70edfdf28025fed
xinhuan <xinhuan@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2008-04-08 09:12:14 +0000

    HandyNotes_Directions: Tag the Ace3 branch to trunk.

---
+++ tmp/trunk/HandyNotes_Directions/koKR.lua
@@ -0,0 +1,6 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_Directions", "koKR")
+if not L then return end
+L["A profession trainer"] = "전문기술 상급자"
+L["Trainer: "] = "전문기술: "
+L["A class trainer"] = "직업 상급자"
+L["Class: "] = "직업: "
---
+++ tmp/trunk/HandyNotes_Directions/zhCN.lua
@@ -0,0 +1,7 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_Directions", "zhCN")
+if not L then return end
+
+L["A profession trainer"] = "专业训练师"
+L["Trainer: "] = "训练师："
+L["A class trainer"] = "职业训练师"
+L["Class: "] = "职业："
