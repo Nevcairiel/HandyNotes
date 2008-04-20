fc7321fe5bf02de1be88fea58903d5d4c3fdc7a8
next96 <next96@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2008-04-20 02:54:17 +0000

    HandyNotes_Directions : - Update locale koKR

--- tmp/trunk/HandyNotes_Directions/koKR.lua
+++ tmp/trunk/HandyNotes_Directions/koKR.lua
@@ -1,6 +1,10 @@
 ﻿local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_Directions", "koKR")
 if not L then return end
-L["A profession trainer"] = "전문기술 상급자"
+L["Profession Trainer"] = "전문기술 상급자"
+L["Class Trainer"] = "직업 상급자"
 L["Trainer: "] = "전문기술: "
-L["A class trainer"] = "직업 상급자"
-L["Class: "] = "직업: "
+
+L["Alliance Battlemasters"] = "얼라이언스 전투사령관"
+L[": Alliance"] = ": 얼라이언스"
+L["Horde Battlemasters"] = "호드 전투사령관"
+L[": Horde"] = ": 호드"
