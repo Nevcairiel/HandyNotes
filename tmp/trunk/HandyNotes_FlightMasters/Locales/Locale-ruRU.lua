9d27dddc38274faf411a28a1f7be04b9273ae5c3
root <root@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2008-09-29 20:58:10 +0000

    removing /tmp

--- tmp/trunk/HandyNotes_FlightMasters/Locales/Locale-ruRU.lua
+++
@@ -1,8 +0,0 @@
-﻿-- HandyNotes_FlightMasters
--- ruRU Localization file
-
-local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_FlightMasters", "ruRU")
-if not L then return end
-
-
--- vim: ts=4 noexpandtab
--- tmp/trunk/HandyNotes_FlightMasters/Locales/Locale-zhTW.lua
+++
@@ -1,8 +0,0 @@
-﻿-- HandyNotes_FlightMasters
--- zhTW Localization file
-
-local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes_FlightMasters", "zhTW")
-if not L then return end
-
-
--- vim: ts=4 noexpandtab
