af102b2ec05b0f2e0599e4fbb989ca21b55c1d17
xinhuan <xinhuan@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2008-11-15 06:02:43 +0000

    Tag v1.1.2 for release. Fixes C nil errors.

---
+++ tags/v1.1.2/Locales/Locale-deDE.lua
@@ -0,0 +1,46 @@
+﻿-- HandyNotes
+-- deDE Localization file
+
+local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes", "deDE")
+if not L then return end
+
+L["HandyNotes"] = "HandyNotes"
+
+L["Enable HandyNotes"] = "HandyNotes aktivieren"
+L["Enable or disable HandyNotes"] = "HandyNotes aktivieren oder deaktivieren"
+
+L["Overall settings"] = "Allgemeine Einstellungen"
+L["Overall settings that affect every database"] = "Allgemeine Einstellungen für jede Datenbank"
+L["These settings control the look and feel of HandyNotes globally. The icon's scale and alpha here are multiplied with the plugin's scale and alpha."] = "Diese Einstellungen legen das allgemeine Aussehen der HandyNotes fest. Die Skalierung und Transparenz der Icons werden mit der Skalierung und Transparenz der Plugin Einstellungen multipliziert."
+L["World Map Icon Scale"] = "Skalierung der Weltkarten-Icons"
+L["The overall scale of the icons on the World Map"] = "Die allgemeine Skalierung der Icons auf der Weltkarte"
+L["World Map Icon Alpha"] = "Transparenz der Weltkarten-Icons"
+L["The overall alpha transparency of the icons on the World Map"] = "Die allgemeine Transparenz der Icons auf der Weltkarte"
+L["Minimap Icon Scale"] = "Skalierung der Minikarten-Icons"
+L["The overall scale of the icons on the Minimap"] = "Die allgemeine Skalierung der Icons auf der Minikarte"
+L["Minimap Icon Alpha"] = "Transparenz der Minikarten-Icons"
+L["The overall alpha transparency of the icons on the Minimap"] = "Die allgemeine Transparenz der Icons auf der Minikarte"
+
+L["Plugins"] = "Plugins"
+L["Plugin databases"] = "Plugin Datenbanken"
+L["Configuration for each individual plugin database."] = "Konfiguration für jede Plugin Datenbank."
+
+L["Add Handy Note"] = "Notiz hinzufügen"
+L["Edit Handy Note"] = "Notiz bearbeiten"
+L["Delete Handy Note"] = "Notiz löschen"
+L["Title"] = "Titel"
+L["Description/Notes:"] = "Beschreibung/Notizen:"
+L["Show on continent map"] = "Auf Kontinentalkarte zeigen"
+L["Add this location to Cartographer_Waypoints"] = "Diese Stelle zu Cartographer_Waypoints hinzufügen"
+L["Add this location to TomTom waypoints"] = "Diese Stelle zu TomTom hinzufügen"
+L["|cFF00FF00Hint: |cffeda55fCtrl+Shift+LeftDrag|cFF00FF00 to move a note"] = "|cFF00FF00Hinweis: |cffeda55fStrg+Shift+Linksklick|cFF00FF00 um eine Notiz zu bewegen"
+
+L["These settings control the look and feel of the HandyNotes icons."] = "Diese Einstellungen legen das Aussehen der HandyNotes Icons fest."
+L["Icon Scale"] = "Icon-Skalierung"
+L["The scale of the icons"] = "Die Skalierung der Icons"
+L["Icon Alpha"] = "Icon-Transparenz"
+L["The alpha transparency of the icons"] = "Die Transparenz der Icons"
+
+L["\nAlt+Right Click To Add a HandyNote"] = "\nAlt+Rechtsklick um ein HandyNote hinzuzufügen"
+
+-- vim: ts=4 noexpandtab
