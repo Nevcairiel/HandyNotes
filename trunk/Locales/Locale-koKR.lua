6b671664fc0b6369cf14e251953a16754087a141
nevcairiel <nevcairiel@c7452a59-836f-40e4-bdfc-1fbc786e764d>
2017-07-12 12:18:31 +0000

    Switch to ignoring missing locale strings

--- trunk/Locales/Locale-koKR.lua
+++ trunk/Locales/Locale-koKR.lua
@@ -5,4 +5,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes", "koKR")
 if not L then return end
 
---@localization(locale="koKR", format="lua_additive_table", table-name="L", handle-unlocalized="comment")@
+--@localization(locale="koKR", format="lua_additive_table", table-name="L", handle-unlocalized="ignore")@
--- trunk/Locales/Locale-zhCN.lua
+++ trunk/Locales/Locale-zhCN.lua
@@ -5,4 +5,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("HandyNotes", "zhCN")
 if not L then return end
 
---@localization(locale="zhCN", format="lua_additive_table", table-name="L", handle-unlocalized="comment")@
+--@localization(locale="zhCN", format="lua_additive_table", table-name="L", handle-unlocalized="ignore")@
