e53288fe3c6ceae183789e73d31667981a0a102f
Hendrik Leppkes <h.leppkes@gmail.com>
2018-07-18 11:25:26 +0000

    Remove AceHook, its unused

--- HandyNotes_HandyNotes.lua
+++ HandyNotes_HandyNotes.lua
@@ -1,7 +1,7 @@
 ﻿---------------------------------------------------------
 -- Module declaration
 local HandyNotes = LibStub("AceAddon-3.0"):GetAddon("HandyNotes")
-local HN = HandyNotes:NewModule("HandyNotes", "AceEvent-3.0", "AceHook-3.0", "AceConsole-3.0")
+local HN = HandyNotes:NewModule("HandyNotes", "AceEvent-3.0", "AceConsole-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("HandyNotes", false)
 
 local HBD = LibStub("HereBeDragons-2.0")
